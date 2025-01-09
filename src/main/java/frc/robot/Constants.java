159364ff8bb31c46e7d849a980c58c332cb92b77
Whitmore Lake Robotics <robotics@wlps.net>
2025-01-08 20:21:09 -0500

    mpg - drive team v1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,6 +11,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import swervelib.math.Matter;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
@@ -28,5 +32,29 @@ public class Constants {
     *   public static final int kRightMotor2Port = 3; 
     * }
     */ 
+
+  
+    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+    public static final double MAX_SPEED  = Units.feetToMeters(15.39);   // need to update 
+
+
+public static final class DrivebaseConstants
+{
+
+  // Hold time on motor brakes when disabled
+  public static final double WHEEL_LOCK_TIME = 10; // seconds
 }
 
+public static class OperatorConstants
+{
+
+  // Joystick Deadband
+  public static final double DEADBAND        = 0.1;
+  public static final double LEFT_Y_DEADBAND = 0.1;
+  public static final double RIGHT_X_DEADBAND = 0.1;
+  public static final double TURN_CONSTANT    = 6;
+}
+
+}
