cbe1083e01e64c5e5aff66ef3c98d3794bd160b8
Whitmore Lake Robotics <robotics@wlps.net>
2025-01-10 20:47:35 -0500

    teg updated vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -338,29 +338,31 @@ public class Vision
     /**
      * Left Camera
      */
-    LEFT_CAM("left",
+   /*  LEFT_CAM("left",
              new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
              new Translation3d(Units.inchesToMeters(12.056),
                                Units.inchesToMeters(10.981),
                                Units.inchesToMeters(8.44)),
              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+             */
     /**
      * Right Camera
      */
-    RIGHT_CAM("right",
+   /*  RIGHT_CAM("right",
               new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
               new Translation3d(Units.inchesToMeters(12.056),
                                 Units.inchesToMeters(-10.981),
                                 Units.inchesToMeters(8.44)),
               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+              */
     /**
      * Center Camera
      */
-    CENTER_CAM("center",
-               new Rotation3d(0, Units.degreesToRadians(18), 0),
-               new Translation3d(Units.inchesToMeters(-4.628),
-                                 Units.inchesToMeters(-10.687),
-                                 Units.inchesToMeters(16.129)),
+    CENTER_CAM("cam11a",
+               new Rotation3d(0, Units.degreesToRadians(45), 0),
+               new Translation3d(Units.inchesToMeters(8.1),
+                                 Units.inchesToMeters(0),
+                                 Units.inchesToMeters(8.4)),
                VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
     /**
